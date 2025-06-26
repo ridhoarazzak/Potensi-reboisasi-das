b6af499f05a5a612a4a2e433af6b954bf96ed60b
ridhoarazzak <54082062+ridhoarazzak@users.noreply.github.com>
2025-06-26 17:11:34 +0700

    Update script.js
    Commit update

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 window.onload = () => {
-  const map = L.map('map').setView([-1.5785, 101.3123], 12);
+  const map = L.map('map').setView([-1.5, 101.3], 11);
 
-  // Basemap OpenStreetMap
-  const osm = L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    { attribution: '© OpenStreetMap contributors' }
+  // Basemap dari Esri
+  const esri = L.tileLayer(
+    'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    {
+      attribution: 'Tiles © Esri'
+    }
   ).addTo(map);
 
-  // Tile GEE - URL terbaru
-  const geeUrl = "https://earthengine.googleapis.com/v1/projects/ee-mrgridhoarazzak/maps/142476407c2433866290dce1c3315b86-3d1cef12768ed2887ee2d58bc490e3f3/tiles/{z}/{x}/{y}";
-
-  const konservasiLayer = L.tileLayer(geeUrl, {
-    attribution: "Google Earth Engine",
-    opacity: 0.6
-  }).addTo(map);
-
-  // Legenda
-  L.control({ position: 'bottomright' }).onAdd = () => {
-    const div = L.DomUtil.create('div', 'legend');
-    div.innerHTML = `<strong>Legenda</strong><br>
-      <i style="background: green; width: 10px; height: 10px; display: inline-block;"></i> Area Konservasi DAS`;
-    return div;
-  }.addTo(map);
+  // Tile layer dari Google Earth Engine
+  const geeTile = L.tileLayer(
+    'https://earthengine.googleapis.com/v1/projects/ee-mrgridhoarazzak/maps/142476407c2433866290dce1c3315b86-007844e5bd78416cb3da0c3a9c4a6eb4/tiles/{z}/{x}/{y}',
+    {
+      attribution: 'Data: Google Earth Engine'
+    }
+  ).addTo(map);
 };
